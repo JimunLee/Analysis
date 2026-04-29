bb4a22e26ed5fe59ac52b529b7e05b60e953469d
jimun_lee <jimun.lee@cern.ch>
2026-04-29 15:55:27 +0900

    oo MC run script

---
+++ OOMC/new_run_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+o2-analysis-propagationservice -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-trackselection -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-ft0-corrected-table -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tpc-service -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-event-selection-service -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error | \
+o2-analysis-multcenttable -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+#o2-analysis-pid-tof-merge -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tof-full -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tof-base -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-lf-kstarinoo -b  --configuration json:///Users/jimun/Analysis/OOMC/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error
