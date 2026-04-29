e07a82211f1b78f2b18c1bc5b62cc9542c9de32a
jimun_lee <jimun.lee@cern.ch>
2026-04-29 15:52:30 +0900

    data run script

---
+++ JetData/new_run_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+o2-analysis-propagationservice -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-tracks-extra-v002-converter -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-event-selection-service -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-je-jet-deriveddata-producer -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-multcenttable -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-trackselection -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tpc-service -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tof-full -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-pid-tof-base -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-je-jet-finder-data-charged -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error  | \
+o2-analysis-lf-kstarinoo -b  --configuration json:///Users/jimun/Analysis/JetData/new-my-config.json --aod-memory-rate-limit 1000000000 --shm-segment-size 8000000000 --min-failure-level error
